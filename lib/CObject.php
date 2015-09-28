88d91a6a26873f7ad5d393303623288ec2440504
shumer596 <shumerok@gmail.com>
2015-09-28 14:19:01 +0300

    edti1

--- lib/CObject.php
+++ lib/CObject.php
@@ -1,94 +1,48 @@
 <?php
 class CObject
 {
-    protected $data = array();
+    protected $_data = array();
 
     public function getData($key = NULL)
     {
-        if ($key && isset($this->data[$key])) {
+        if (isset($this->_data[$key])) {
 
-            return $this->data[$key];
-
+            return $this->_data[$key];
 
         } else
-            return $this->data;
-
-
+            return $this->_data;
     }
 
-    public function setData($key, $value)
+    public function setData($value,$key = NULL)
     {
-        if ($key && $value) {
-            $this->data[$key] = $value;
+        if (isset($key) && $value ){
+
+            $this->_data[$key] = $value;
+
         } else {
-            return $this;
+
+            $this->_data[]=$value;
 
         }
-
         return $this;
     }
 
-    public function __call($name, $args)
-    {
+   
 
-
-        switch (substr($name, 0, 3)) {
-
-            case 'set':
-                $name = $this->_camelCase($name);
-                $this->setData($name, $args[0]);
-//                var_dump($this->data);
-                return $this;
-
-            case 'get':
-                return $this->getData($this->_camelCase($name));
-
-            case 'has':
-                $name = $this->_camelCase($name);
-                if (array_key_exists($this->_camelCase($name), $this->data)) {
-                    return $this;
-                } else {
-                    return $this;
-                }
-
-            case 'uns':
-                $name = $this->_camelCase($name);
-                if (array_key_exists($this->_camelCase($name), $this->data)) {
-                    $this->unsetData($this->data[$name]);
-                } else {
-                    return $this;
-                }
-
-
-        }
-
-        return $this;
-    }
-
-    public function unsetData($var)
-    {
-
-        if (isset($this->data[$var])) {
-            unset($this->data[$var]);
-        } else {
-            return $this;
-        }
-
-    }
-
-
-    protected function _camelCase($name)
-    {
-        $name = substr($name, 3);
-        $name = ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', $name)), '_');
-        return $name;
-    }
+    // protected function _camelCase($name)
+    // {
+    //     $name = substr($name, 3);
+    //     $name = ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', $name)), '_');
+    //     return $name;
+    // }
 
 
 }
-
-
 $basa = new CObject();
-$basa->setCustomerName('artem','kozhuh');
-//$basa->getCustomerName();
-var_dump($basa->getData(), $basa->getCustomerName());
+$basa->setData('Artem','Kozhuh');
+$basa->setData('Artem1','Kozhuh1');
+$basa->setData('Shumer');
+echo "<pre>";
+print_r($basa->getData());
+var_dump($basa->getData('0'));
+echo "</pre>";
