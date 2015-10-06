ef9c854404e55106409bc3dc258c0c3080282992
shumer596 <shumerok@gmail.com>
2015-10-06 16:48:52 +0300

    Router2

--- app/Core/Controller/Router.php
+++ app/Core/Controller/Router.php
@@ -3,50 +3,29 @@ class Core_Controller_Router
 {
  public static function dispatch()
  	{
- 		$uri = explode('/', $_SERVER['REQUEST_URI']);
-
- 		$controller_name = 'Index';
-		$action_name = 'index';
-		
-		$routes = explode('/', $_SERVER['REQUEST_URI']);
-
-		// get controller's name
-		if ( !empty($routes[1]) )
-		{	
-			$controller_name = $routes[1];
-		}
-		
-		// get action name
-		if ( !empty($routes[2]) )
-		{
-			$action_name = $routes[2];
-		}
-
-		$controller_name = 	$controller_name . 'Controller';
-		$action_name  	 = 	$action_name . 'Action';
-
-		var_dump($controller_name);
-		echo "<br />";
-		var_dump($action_name);
-
-		$controller_file = $controller_name.'.php';
-		$controller_path = 'app/Core/controllers/'.$controller_file;
-		if(file_exists($controller_path))
-		{
-			include 'app/Core/controllers/'.$controller_file;
-		}
-		else
-		{
-			Core_Controller_Router::ErrorPage404($controller_path);
-		}
-
-
-   		
+	$f = preg_split("[/]", $_SERVER['PATH_INFO'], -1, PREG_SPLIT_NO_EMPTY);
+	echo "<pre>";
+	print_r($f);
+	for ($i=0; $i <3 ; $i++) { 
+		array_shift($f);
 	}
+	print_r($f);
+   	$b = array();
 
-	static function ErrorPage404($param)
-	{
-        echo "not found -> $param";
-    }
+   	$array =$f;
+	$result = array();
+	while ( $array ) {
+    	$name = array_shift( $array );
+    	$value = array_shift( $array );
+    	$result[$name] = $value;
+}
 
+var_export( $result );
+	
+	// static function ErrorPage404($param)
+	// {
+ //        echo "not found -> $param";
+ //      }
+      
+	}
 }
