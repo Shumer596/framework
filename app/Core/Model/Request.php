e87755ce25e1f4663c640d12732cd0b1f87cbc50
Shumer596 <ghost12@mail.ru>
2015-10-08 20:30:24 +0300

    Request

--- app/Core/Model/Request.php
+++ app/Core/Model/Request.php
@@ -1,17 +1,31 @@
 <?php
-class Core_Model_Request extends CObject 
+class Core_Model_Request extends CObject
 {
-    
 
 
+
+    /**
+     *  @setModule() return module path
+     */
     public function __construct()
     {
        $request = preg_split("[/]", $_SERVER['PATH_INFO'], -1, PREG_SPLIT_NO_EMPTY);
         echo "<pre>";
-        
-        $this->setModule($request[0]);
-        $this->setController($request[1]);
-        $this->setAction($request[2]);
+
+        if(isset($request[0]))
+        {
+            $this->setModule($request[0]);
+        }
+
+        if(isset($request[1]))
+        {
+            $this->setController($request[1]);
+        }
+
+        if(isset($request[2]))
+        {
+            $this->setAction($request[2]);
+        }
 
         	for ($i=0; $i <3 ; $i++) 
         	{ 
@@ -20,15 +34,19 @@ class Core_Model_Request extends CObject
          	}
 
         // get second part of request
-        $array =$request;
-        $result = array();
+        if (isset($request[0]))
+        {
+            $array =$request;
+            $result = array();
             while ( $array ) {
-            $name = array_shift( $array );
-            $value = array_shift( $array );
-            $result[$name] = $value; 
-   	 }
+                $name = array_shift( $array );
+                $value = array_shift( $array );
+                $result[$name] = $value;
+            }
 
-    	$this->setParams($result);
+            $this->setParams($result);
+        }
+
     	print_r(($this->getData()));
         /* cut your request here */
     }
