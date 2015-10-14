8be0a87359b5653c729f6c2b1cd32784a0519608
Shumer596 <ghost12@mail.ru>
2015-10-14 17:41:42 +0300

    Singleton

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ App::run();
 
 /* @var $db Zend_Db_Adapter_Pdo_Mysql */
 $db = App::registry('write_connection');
-var_dump($db);
+var_dump(App::getSingleton('core/request'));
+var_dump(App::getSingleton('core/request'));
 $select = new Zend_Db_Select($db);
 
-//$select->from('blog_post');
-//$result = $db->query($select);
-//var_dump($result->fetchAll());
+$select->from('blog_post');
+$result = $db->query($select);
+var_dump($result->fetchAll());
 //$connect = App::getModel('core/resource');
 
